f510b960f479485c49a39d48d4b063414499ddbe
Daniel Jimenez <cjimenez187@aol.com>
2021-08-21 23:47:15 -0500

    Final TidyTuesday

--- Data-screencasts/Olympics.R
+++ Data-screencasts/Olympics.R
@@ -10,7 +10,12 @@ library(tidyverse)
 library(tidytext)
 library(infer)
 library(scales)
+library(corrplot)
+library(broom)
+library(ggpubr)
+library(car)
 theme_set(theme_bw())
+
 
 ## Data ---------
 
@@ -34,6 +39,7 @@ olympics %>%
 
 
 # Some little Feature Engineering
+
 olympics<- olympics %>%
     mutate(sex = ifelse(sex == 'M','Male','Female')) 
 
@@ -138,15 +144,227 @@ olympics %>%
 
 
 
-### Infer model ----
+### Experimental Design model ----
 
 olympics %>%
-    filter(!is.na(medal),
-        !is.na(sex),
-        !is.na(age),
-        sport == 'Swimming')  %>%
-        specify(medal ~ sex ) %>%
-        hypothesize(null = 'independence') %>%
-        generate(reps = 5, type = "permute") %>%
-        calculate("diff in means",order = c('Female','Male'))
+    na.omit()%>%
+    select(c(age,height,weight)) %>%
+    cor()
+
+
+#              age     height    weight
+# age    1.00000000 0.09594238 0.1617503
+# height 0.09594238 1.00000000 0.8018308
+# weight 0.16175032 0.80183082 1.0000000
         
+olympics %>%
+    na.omit()%>%
+    select(c(age,height,weight)) %>%
+    cor() %>%
+    corrplot(method = 'pie',
+    title = 'Principal Correlations',
+    order = 'hclust') 
+
+fit <- olympics %>%
+       na.omit()%>%
+       select(c(age,height,weight)) %>%
+       lm(weight ~ ., data = .)
+
+fit %>%
+    plot()
+
+
+ 
+
+fit <- olympics %>%
+       na.omit()%>%
+       select(c(age,height,weight,sex)) %>% 
+       lm(weight ~ age + height + sex, data = .)
+
+Experimental <- olympics %>%
+                na.omit()%>%
+                select(c(age,height,weight,sex))
+
+Experimental$predicted <- predict(fit)
+Experimental$residuals <- residuals(fit)
+
+quartz()
+Experimental %>%
+sample_n(150) %>%
+    ggplot(aes(age,weight, color = sex)) +
+    geom_segment(aes(xend = age,
+                 yend = predicted),
+                 color = 'gray') +
+    geom_point(alpha = .2) +
+    geom_point(aes(y = predicted), shape = 1)+
+    facet_wrap(~sex, scales = 'free')
+
+# Its seems that 'Male' have more correlation  with the weigth and age
+
+Experimental %>%
+    ggplot(aes(height,
+    weight, 
+    color = sex)) +
+    geom_segment(aes(xend = height,
+                 yend = predicted),
+                 color = 'gray') +
+    geom_point(alpha = .2) +
+    geom_point(aes(y = predicted), shape = 2, color = 'black')+
+    facet_wrap(~sex, scales = 'free')
+
+# The heigth and age (combination) are weak explains 
+Experimental %>%
+    ggplot(aes(height,
+    weight, 
+    color = sex)) +
+    geom_smooth(method = 'lm',
+                se = FALSE,
+                color = 'gray')+
+    geom_segment(aes(xend = height,
+                 yend = predicted),
+                 color = 'purple') +
+    geom_point() +
+    geom_point(aes(y = predicted), shape = 1, color = 'black')+
+    facet_wrap(~sex, scales = 'free')
+
+
+## Residuals problems
+set.seed(12345)
+olympics %>%
+       na.omit() %>% 
+       distinct(name,.keep_all = T) %>%
+       lm(age ~ height + weight, data = .) %>%
+       augment() %>%
+       sample_n(150) %>% 
+       ggplot(aes(weight,age)) +
+           geom_smooth(method = 'lm',
+                se = FALSE,
+                color = 'gray')+
+    geom_segment(aes(xend = weight,
+                 yend = .fitted),
+                 color = 'red') +
+       geom_point() +
+       geom_point(aes (x=weight,
+                  y = .fitted), shape = 1)
+
+# is possible that problem (predicted age) have heterocesaticy behavior?? 
+set.seed(1234)
+Female_BH <- olympics %>%
+    filter(sex == 'Female') %>%
+       na.omit() %>% 
+       distinct(name,.keep_all = T) %>%
+       lm(age ~ height + weight, data = .) %>%
+       augment() %>%
+       sample_n(150) %>% 
+       ggplot(aes(weight,age)) +
+           geom_smooth(method = 'lm',
+                se = FALSE,
+                color = 'gray')+
+    geom_segment(aes(xend = weight,
+                 yend = .fitted),
+                 color = 'red') +
+       geom_point() +
+       geom_point(aes (x=weight,
+                  y = .fitted), shape = 1)+
+    expand_limits(x = 0) +
+    labs(title = 'Female predict Model (Age) with adj Values')
+
+
+set.seed(1234)
+Male_Bh<-olympics %>%
+    filter(sex != 'Female') %>%
+       na.omit() %>% 
+       distinct(name,.keep_all = T) %>%
+       lm(age ~ height + weight, data = .) %>%
+       augment() %>%
+       sample_n(150) %>% 
+       ggplot(aes(weight,age)) +
+           geom_smooth(method = 'lm',
+                se = FALSE,
+                color = 'gray')+
+    geom_segment(aes(xend = weight,
+                 yend = .fitted),
+                 color = 'red') +
+       geom_point() +
+       geom_point(aes (x=weight,
+                  y = .fitted), shape = 1) +
+       expand_limits(x = 0) +
+       labs(title = 'Male predict Model (Age) with adj Values')
+
+
+
+ggarrange(Female_BH,Male_Bh)
+
+# Yes!!!! the gender generate heterosedasticy, but .....
+
+## A Test ------
+
+model <- olympics %>%
+      select(c(age , height , weight)) %>%
+       na.omit() %>% 
+       distinct(.keep_all = T) %>%
+       lm(age ~ height + weight, data = .) 
+
+data <- olympics %>%
+      select(c(age , height , weight, sex, sport)) %>%
+       na.omit() %>% 
+       distinct(.keep_all = T)      
+
+data$residuals <- residuals(model)
+
+
+lmtest::bptest(model)  # Breusch-Pagan test
+
+#        studentized Breusch-Pagan test
+
+# data:  model
+# BP = 399.75, df = 2, p-value < 2.2e-16
+
+car::ncvTest(model) 
+
+# Non-constant Variance Score Test 
+# Variance formula: ~ fitted.values 
+# Chisquare = 178.0335, Df = 1, p = < 2.22e-16
+
+
+# the test confirms heteroscedasticity presence!!!
+
+# Last test -----
+
+data %>%
+    ggplot(aes(height,residuals)) +
+    geom_point(col = 'gray') +
+    geom_abline(slope = 0)
+
+var.func <- lm(residuals^2 ~ height, data = data)
+summary(var.func)
+
+qchisq(.95, df = 1)
+
+
+a<-lmtest::bptest(model)
+
+BP = a$statistic %>% .[[1]]
+
+BP > qchisq(.95, df = 1)
+
+# we reject the null hypothesis and conclude that heterocedasticity is present.
+
+## Inferencial Test ------
+# The Age of a athletes is independent for sport ??
+
+F_hat <- data %>% 
+  specify(age ~ sport) %>%
+  calculate(stat = "F")
+
+null_f_distn_theoretical <- data %>%
+   specify(age ~ sport) %>%
+   hypothesize(null = "independence") %>%
+   calculate(stat = "F")
+
+
+
+visualize(null_f_distn_theoretical, method = "theoretical") +
+shade_p_value(obs_stat = F_hat, direction = "greater")
+
+# reject Null Hypothesis : Age have effects sport selections
